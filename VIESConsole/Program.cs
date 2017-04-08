5f1581955544529eb7093c5edad7e3ae6ae8d93d
emuijs <eric.muijs@gmail.com>
2017-04-08 11:12:33 +0200

    Forked

---
+++ VIESConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VIESCheckVatService;
+
+namespace VIESConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VIESCheckVatService.IVIESCheckVatService iCheckVatService =
+            VIESCheckVatService.VIESCheckServiceFactory.getService(ServiceType.SOAP);
+
+            while(true)
+            {
+                Console.WriteLine("VAT Country?");
+                string VAT_country = Console.ReadLine();
+                Console.WriteLine("VAT Number?");
+                string VAT_number = Console.ReadLine();
+                VIESCheckVatService.VIESVatModel vatModel = iCheckVatService.getDetailVAT(VAT_country, VAT_number);
+                Console.Out.WriteLine("Valid: " + vatModel.Valid);
+                Console.Out.WriteLine("County Code: " + vatModel.CountryCode);
+                Console.Out.WriteLine("VAT Number: " + vatModel.VatNumber);
+                Console.Out.WriteLine("Request Date: " + vatModel.RequestDate);
+                Console.Out.WriteLine("Org. Name: " + vatModel.Name);
+                Console.Out.WriteLine("Org. Address: " + vatModel.Address);                
+            }
+            
+
+            
+        }
+    }
+}
