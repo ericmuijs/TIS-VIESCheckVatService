532d51b74d8878f3b8f53b4fec3f10e73a4a27ce
emuijs <eric.muijs@gmail.com>
2017-04-09 07:20:25 +0200

    Catch in console

--- VIESConsole/Program.cs
+++ VIESConsole/Program.cs
@@ -11,26 +11,37 @@ namespace VIESConsole
     {
         static void Main(string[] args)
         {
-            VIESCheckVatService.IVIESCheckVatService iCheckVatService =
-            VIESCheckVatService.VIESCheckServiceFactory.getService(ServiceType.SOAP);
-
-            while(true)
+            try
             {
-                Console.WriteLine("VAT Country?");
-                string VAT_country = Console.ReadLine();
-                Console.WriteLine("VAT Number?");
-                string VAT_number = Console.ReadLine();
-                VIESCheckVatService.VIESVatModel vatModel = iCheckVatService.getDetailVAT(VAT_country, VAT_number);
-                Console.Out.WriteLine("Valid: " + vatModel.Valid);
-                Console.Out.WriteLine("County Code: " + vatModel.CountryCode);
-                Console.Out.WriteLine("VAT Number: " + vatModel.VatNumber);
-                Console.Out.WriteLine("Request Date: " + vatModel.RequestDate);
-                Console.Out.WriteLine("Org. Name: " + vatModel.Name);
-                Console.Out.WriteLine("Org. Address: " + vatModel.Address);                
-            }
-            
+                VIESCheckVatService.IVIESCheckVatService iCheckVatService =
+                   VIESCheckVatService.VIESCheckServiceFactory.getService(ServiceType.SOAP);
 
-            
+                while (true)
+                {
+                    Console.WriteLine("VAT Country?");
+                    string VAT_country = Console.ReadLine();
+                    Console.WriteLine("VAT Number?");
+                    string VAT_number = Console.ReadLine();
+
+                    VIESCheckVatService.VIESVatModel vatModel = iCheckVatService.getDetailVAT(VAT_country, VAT_number);
+
+                    Console.Out.WriteLine("Valid: " + vatModel.Valid);
+                    Console.Out.WriteLine("County Code: " + vatModel.CountryCode);
+                    Console.Out.WriteLine("VAT Number: " + vatModel.VatNumber);
+                    Console.Out.WriteLine("Request Date: " + vatModel.RequestDate);
+                    Console.Out.WriteLine("Org. Name: " + vatModel.Name);
+                    Console.Out.WriteLine("Org. Address: " + vatModel.Address);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(
+                    "The service has responded with an error. " + ex.Message,
+                    "Service error occurred");
+                Console.ReadKey();
+
+            }
+           
         }
     }
 }
